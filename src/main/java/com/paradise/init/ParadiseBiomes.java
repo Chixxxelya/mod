72c8972c013cec3df5d2437c87ed9dcea6738b8f
Echo <cixxelya@gmail.com>
2026-04-02 21:46:49 +0300

    Update ParadiseBiomes.java

--- src/main/java/com/paradise/init/ParadiseBiomes.java
+++ src/main/java/com/paradise/init/ParadiseBiomes.java
@@ -1,16 +1,10 @@
 package com.paradise.init;
 
 import com.paradise.ParadiseDimensionMod;
-import net.minecraft.registry.Registerable;
-import net.minecraft.registry.RegistryEntry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
-import net.minecraft.sound.MusicType;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.biome.Biome;
-import net.minecraft.world.biome.BiomeEffects;
-import net.minecraft.world.biome.GenerationSettings;
-import net.minecraft.world.biome.SpawnSettings;
 
 public class ParadiseBiomes {
     public static final RegistryKey<Biome> PARADISE_ISLANDS = RegistryKey.of(
@@ -19,10 +13,5 @@ public class ParadiseBiomes {
     );
 
     public static void register() {
-        // Registration handled through data files
-    }
-
-    public static void bootstrap(Registerable<Biome> registry) {
-        // Registration is handled through JSON data files
     }
 }
