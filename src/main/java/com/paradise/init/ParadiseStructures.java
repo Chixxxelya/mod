f793f734bd9fc73d8fd68deef31a9c154f28173f
Echo <cixxelya@gmail.com>
2026-04-02 21:45:45 +0300

    Update ParadiseStructures.java

--- src/main/java/com/paradise/init/ParadiseStructures.java
+++ src/main/java/com/paradise/init/ParadiseStructures.java
@@ -1,24 +1,16 @@
 package com.paradise.init;
 
 import com.paradise.ParadiseDimensionMod;
-import net.minecraft.registry.Registerable;
-import net.minecraft.registry.RegistryEntry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
-import net.minecraft.structure.Structure;
 import net.minecraft.util.Identifier;
 
 public class ParadiseStructures {
-    public static final RegistryKey<Structure> CASTLE = RegistryKey.of(
+    public static final RegistryKey<Object> CASTLE = RegistryKey.of(
         RegistryKeys.STRUCTURE,
         new Identifier(ParadiseDimensionMod.MOD_ID, "castle")
     );
 
     public static void register() {
-        // Registration handled through data files
-    }
-
-    public static void bootstrap(Registerable<Structure> registry) {
-        // Registration is handled through JSON data files
     }
 }
